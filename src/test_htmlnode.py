d2e7015833a2af71ec10684eba5fb473a302892b
Keeroh-jpg <samuel.baugh97@gmail.com>
2025-09-13 23:57:20 +0100

    added 2 more leaf node tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -37,3 +37,12 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_a(self):
         node = LeafNode("a", "Click this!", {"href": "https://www.example.com"} )
         self.assertEqual(node.to_html(), '<a href="https://www.example.com">Click this!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+    
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
