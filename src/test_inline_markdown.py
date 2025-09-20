024a37aa62bff64862624e028e0613faa927f1b2
Keeroh-jpg <samuel.baugh97@gmail.com>
2025-09-20 21:51:45 +0100

    Added a few more complicated tests for split_delimiter

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -14,22 +14,18 @@ class TestInlineMarkdown(unittest.TestCase):
     def test_split_delimiter_code(self):
         node = TextNode("This is a text with a `code block` word", TextType.plain)
         new_nodes = split_nodes_delimiter([node], "`", TextType.code)
+        self.assertListEqual([TextNode("This is a text with a ", TextType.plain),TextNode("code block", TextType.code),TextNode(" word", TextType.plain),],new_nodes,)
 
-    def test_split_delimiter_bold(self):
-        node = TextNode("This is a text with a **bold** word", TextType.plain)
+    def test_split_delimiter_double_bold(self):
+        node = TextNode("This is a text with a **bold** word and **another**", TextType.plain)
         new_nodes = split_nodes_delimiter([node], "**", TextType.bold)
+        self.assertListEqual([TextNode("This is a text with a ", TextType.plain),TextNode("bold", TextType.bold),TextNode(" word and ", TextType.plain),TextNode("another", TextType.bold),],new_nodes)
     
-    def test_split_delimiter_double_delimiter(self):
-        node = TextNode("This is a text with a **** word", TextType.plain)
+    def test_split_delimiter_bold_and_italic(self):
+        node = TextNode("This is a text with an __italic__ word and a **bold** word", TextType.plain)
         new_nodes = split_nodes_delimiter([node], "**", TextType.bold)
-    
-    def test_split_delimiter_no_text(self):
-        node = TextNode("****", TextType.plain)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.bold)
-    
-    def test_split_delimiter_unbalanced_delimiters(self):
-        node = TextNode("This is a **bold** word", TextType.plain)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.bold)
+        new_nodes = split_nodes_delimiter(new_nodes, "__", TextType.italic)
+        self.assertListEqual([TextNode("This is a text with an ", TextType.plain),TextNode("italic", TextType.italic),TextNode(" word and a ", TextType.plain),TextNode("bold", TextType.bold),TextNode(" word", TextType.plain),],new_nodes)
 
 
 
