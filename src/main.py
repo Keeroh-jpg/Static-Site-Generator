2d94860732c87b20b6519821310dd69474625ea7
Keeroh-jpg <samuel.baugh97@gmail.com>
2025-12-19 18:50:21 +0000

    started the copy static syntax and created test files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+import errno
 
 def main():
-    text_node = TextNode("Hello, World!", TextType.link, "https://www.boot.dev")
-    print(text_node)
-
+    path = "~/workspace/github.com/Keeroh-jpg/Static-Site-Generator/"
+    src = "~/workspace/github.com/Keeroh-jpg/Static-Site-Generator/static"
+    dest = "~/workspace/github.com/Keeroh-jpg/Static-Site-Generator/public"
 main()
